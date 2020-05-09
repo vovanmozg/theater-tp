7b1e04c28cce10df6ca3ac77c25f2d6d993fc175
Vladimir Polukhin <vovanmozg@gmail.com>
2020-05-09 13:39:35 +0300

    Add init db data

---
+++ db/init.sql
@@ -0,0 +1,7 @@
+CREATE USER rails;
+CREATE DATABASE theater_prod;
+CREATE DATABASE theater_dev;
+CREATE DATABASE theater_test;
+GRANT ALL PRIVILEGES ON DATABASE theater_prod TO rails;
+GRANT ALL PRIVILEGES ON DATABASE theater_dev TO rails;
+GRANT ALL PRIVILEGES ON DATABASE theater_test TO rails;
